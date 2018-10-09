b4d80d8227681820ff87729d106c7fca45c080be
msinn <info1@msinn.de>
2018-10-09 10:04:41 +0200

    Fix for wrong value in item env.system.start

--- lib/env/init.py
+++ lib/env/init.py
@@ -13,5 +13,9 @@ sh.env.system.name(hostname, logic.lname)
 
 # system start
 #start = sh.now() - datetime.timedelta(seconds=psutil.boot_time())
-start = shtime.now() - datetime.timedelta(seconds=psutil.boot_time())
+#start = shtime.now() - datetime.timedelta(seconds=psutil.boot_time())
+#start = shtime.now() - datetime.timedelta(seconds=0)
+#sh.env.system.start(start, logic.lname)
+
+start =datetime.datetime.fromtimestamp(psutil.boot_time()).strftime("%Y-%m-%d %H:%M:%S")
 sh.env.system.start(start, logic.lname)
