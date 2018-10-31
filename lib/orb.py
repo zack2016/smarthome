b5deec2f18bd51f0fce59ee6c942e6df62ec5db2
Bernd Meiners <Bernd.Meiners@mail.de>
2018-10-31 11:29:53 +0100

    more inline documentation to orb.py

--- lib/orb.py
+++ lib/orb.py
@@ -3,6 +3,7 @@
 # vim: set encoding=utf-8 tabstop=4 softtabstop=4 shiftwidth=4 expandtab
 #########################################################################
 # Copyright 2011-2014 Marcus Popp                          marcus@popp.mx
+#
 #########################################################################
 #  This file is part of SmartHomeNG.    https://github.com/smarthomeNG//
 #
@@ -39,10 +40,29 @@ from dateutil.tz import tzutc
 class Orb():
 
     def __init__(self, orb, lon, lat, elev=False):
+        """
+        :param orb: either 'sun' or 'moon'
+        :param lon: longitude of observer in degrees
+        :param lat: latitude of observer in degrees
+        :param elev: elevation of observer in meters
+        """
         if ephem is None:
             logger.warning("Could not find/use ephem!")
             return
         self._obs = ephem.Observer()
+        """
+        PyEphem: An `Observer` instance allows  to compute the positions of
+        celestial bodies as seen from a particular position on the Earth's surface.        
+        Following attributes can be set after creation (used defaults are given):        
+        `date` - the moment the `Observer` is created
+        `lat` - zero degrees latitude
+        `lon` - zero degrees longitude
+        `elevation` - 0 meters above sea level
+        `horizon` - 0 degrees
+        `epoch` - J2000
+        `temp` - 15 degrees Celsius
+        `pressure` - 1010 mBar
+"""
         self._obs.long = str(lon)
         self._obs.lat = str(lat)
         if elev:
@@ -55,6 +75,15 @@ class Orb():
             self.light = self._light
 
     def rise(self, doff=0, moff=0, center=True, dt=None):
+        """
+        Computes the rise of either sun or moon
+        :param doff:    degrees offset for the observers horizon
+        :param moff:    minutes offset from time of rise (either before or after)
+        :param center:  if True then the centerpoint of either sun or moon will be considered to make the transit
+                        otherwise the upper limb will be considered
+        :param dt:      start time for the search for a rise, if not given the current time will be used
+        :return:
+        """
         # workaround if rise is 0.001 seconds in the past
         if dt is not None:
             self._obs.date = dt - dt.utcoffset()
@@ -69,6 +98,15 @@ class Orb():
         return next_rising.replace(tzinfo=tzutc())
 
     def set(self, doff=0, moff=0, center=True, dt=None):
+        """
+        Computes the setting of either sun or moon
+        :param doff:    degrees offset for the observers horizon
+        :param moff:    minutes offset from time of setting (either before or after)
+        :param center:  if True then the centerpoint of either sun or moon will be considered to make the transit
+                        otherwise the upper limb will be considered
+        :param dt:      start time for the search for a setting, if not given the current time will be used
+        :return:
+        """
         # workaround if set is 0.001 seconds in the past
         if dt is not None:
             self._obs.date = dt - dt.utcoffset()
@@ -82,7 +120,15 @@ class Orb():
         next_setting = next_setting + dateutil.relativedelta.relativedelta(minutes=moff)
         return next_setting.replace(tzinfo=tzutc())
 
-    def pos(self, offset=None, degree=False, dt=None):  # offset in minutesA
+    def pos(self, offset=None, degree=False, dt=None):
+        """
+        Calculates the position of either sun or moon
+        :param offset:  given in minutesA, shifts the time of calculation by some minutes back or forth
+        :param degree:  if True: return the position of either sun or moon from the observer as degrees,
+                        otherwise as radians
+        :param dt:      time for which the position needs to be calculated
+        :return:        a tuple with azimuth and elevation
+        """
         if dt is None:
             date = datetime.datetime.utcnow()
         else: 
@@ -96,7 +142,12 @@ class Orb():
         else:
             return (self._orb.az, self._orb.alt)
 
-    def _light(self, offset=None):  # offset in minutes
+    def _light(self, offset=None):
+        """
+        Applies only for moon, returns fraction of lunar surface illuminated when viewed from earth
+        for the current time plus an offset
+        :param offset: an offset given in minutes
+        """
         date = datetime.datetime.utcnow()
         if offset:
             date += dateutil.relativedelta.relativedelta(minutes=offset)
@@ -104,7 +155,12 @@ class Orb():
         self._orb.compute(self._obs)
         return int(round(self._orb.moon_phase * 100))
 
-    def _phase(self, offset=None):  # offset in minutes
+    def _phase(self, offset=None):
+        """
+        Applies only for moon, returns the moon phase related to a cycle of approx. 29.5 days
+        for the current time plus an offset
+        :param offset: an offset given in minutes
+        """
         date = datetime.datetime.utcnow()
         cycle = 29.530588861
         if offset:
