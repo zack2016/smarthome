5428b5f892074e13c64f5508c115366419e4c321
msinn <info1@msinn.de>
2018-11-03 14:05:41 +0100

    lib.item: Made some of the internal data of the item class available as property through inner-class property

--- lib/item.py
+++ lib/item.py
@@ -62,6 +62,7 @@ import threading
 import math
 import json
 from ast import literal_eval
+import inspect
 
 from lib.plugin import Plugins
 from lib.shtime import Shtime
@@ -398,6 +399,8 @@ class Item():
         self._threshold = False
         self._type = None
         self._value = None
+
+        self.property = self.Property(self)
         # history
         # TODO: create history Arrays for some values (value, last_change, last_update  (usage: multiklick,...)
         # self.__history = [None, None, None, None, None]
@@ -720,178 +723,244 @@ class Item():
         return cycle
 
 
+
     """
-    Properties of the item, that can be accessed from outside of the item class
+    --------------------------------------------------------------------------------------------
+    ---
+    ---  Properties of the item, that can be accessed from outside of the item class
+    ---
     """
 
-    @property
-    def xx_enforce_updates(self):
+    class Property:
         """
-        Property: enforce_updates
+        Inner class Property of item class.
 
-        Available in SmartHomeNG v1.6 and above
+        This class encapsulates all properties that are publicly available
 
-        :param value: enforce_update state of the item
-        :type value: bool
-
-        :return: enforce_update state of the item
-        :rtype: bool
+        An instance of this class is created in the __init__ method of the item class
         """
-        return self._enforce_updates
+        _item: None
 
-    @xx_enforce_updates.setter
-    def xx_enforce_updates(self, value):
+        def __init__(self, parent):
+            self._item = parent
 
-        if isinstance(value, bool):
-            self._enforce_updates = value
-        else:
-            logger.error("Cannot set property 'enforce_updates' of item {} to a non-boolean value".format(self.path()))
-        return
+        def _ro_error(self):
+            prop = inspect.stack()[1][3]
+            logger.error("Cannot set readonly property '{}' of item '{}'".format(prop, self._item._path))
+            return
+
+        def _type_error(self, err):
+            prop = inspect.stack()[1][3]
+            logger.error("Cannot set property '{}' of item '{}' to a {} value".format(prop, self._item._path, err))
+            return
+
+        def _cast_warning(self, value):
+            prop = inspect.stack()[1][3]
+            logger.warning("Casting value '{}' to required type before assigning it to property '{}' of item '{}'".format(value, prop, self._item._path))
+            return
 
 
-    @property
-    def xx_trigger(self):
-        """
-        Property: Triggers of the item
+        @property
+        def enforce_updates(self):
+            """
+            Property: enforce_updates
 
-        Available in SmartHomeNG v1.6 and above
+            Available in SmartHomeNG v1.6 and above
 
-        :param value: list of triggers
-        :type value: list
+            :param value: enforce_update state of the item
+            :type value: bool
 
-        :return: [] if not defined or a list of triggers
-        :rtype: list of str
-        """
-        if self._trigger:
-            return self._trigger
-        return []
+            :return: enforce_update state of the item
+            :rtype: bool
+            """
+            return self._item._enforce_updates
 
-    def _checkstrtype(self, obj):
-        return bool(obj) and all(isinstance(elem, str) for elem in obj)
+        @enforce_updates.setter
+        def enforce_updates(self, value):
 
-    @xx_trigger.setter
-    def xx_trigger(self, value):
-
-        if isinstance(value, list):
-            if value == []:
-                self._trigger = False
+            if isinstance(value, bool):
+                self._item._item._enforce_updates = value
             else:
-                if self._checkstrtype(value):
-                    self._trigger = value
+                self._type_error('non-boolean')
+            return
+
+
+        @property
+        def eval(self):
+            """
+            Property: eval expression
+
+            Available in SmartHomeNG v1.6 and above
+
+            :param value: eval expression of the item
+            :type value: str
+
+            :return: eval expression of the item
+            :rtype: str
+            """
+            if self._item._eval:
+                return self._item._eval
+            return ''
+
+        @eval.setter
+        def eval(self, value):
+
+            if isinstance(value, str):
+                if value == '':
+                    self._item._eval = None
                 else:
-                    logger.error("Cannot set property 'trigger' of item {} to a list containing non-string value(s)".format(self.path()))
-        else:
-            logger.error("Cannot set property 'trigger' of item {} to a non-list value".format(self.path()))
-        return
-
-
-    @property
-    def xx_eval(self):
-        """
-        Property: eval expression of the item
-
-        Available in SmartHomeNG v1.6 and above
-
-        :param value: eval expression of the item
-        :type value: str
-
-        :return: eval expression of the item
-        :rtype: str
-        """
-        if self._eval:
-            return self._eval
-        return ''
-
-    @xx_eval.setter
-    def xx_eval(self, value):
-
-        if isinstance(value, str):
-            if value == '':
-                self._eval = None
+                    self._item._eval = value
             else:
-                self._eval = value
-        else:
-            logger.error("Cannot set property 'eval' of item {} to a non-string value".format(self.path()))
-        return
+                self._type_error('non-non-string')
+            return
 
 
-    @property
-    def ipath(self):
-        """
-        Read-Only Property: path
+        @property
+        def name(self):
+            """
+            Property: name
 
-        Available in SmartHomeNG v1.6 and above
+            Available in SmartHomeNG v1.6 and above
 
-        :return: path of the item
-        :rtype: str
-        """
-        return self._path
+            :param value: name of the item
+            :type value: str
 
+            :return: name of the item
+            :rtype: str
+            """
+            return self._item._name
 
-    @property
-    def iname(self):
-        """
-        Property: name
+        @name.setter
+        def name(self, value):
 
-        Available in SmartHomeNG v1.6 and above
-
-        :param value: name of the item
-        :type value: str
-
-        :return: name of the item
-        :rtype: str
-        """
-        return self._name
-
-    @iname.setter
-    def iname(self, value):
-
-        if isinstance(value, str):
+            if not isinstance(value, str):
+                self._cast_warning(value)
+                value = '{}'.format(value)
             if value == '':
-                self._name = self._path
+                self._item._name = self._item._path
             else:
-                self._name = value
-        else:
-            logger.error("Cannot set property 'name' of item {} to a non-string value".format(self.path()))
-        return
+                self._item._name = value
+            return
 
 
-    @property
-    def ivalue(self):
-        """
-        Property: value
+        @property
+        def path(self):
+            """
+            Read-Only Property: path
 
-        Available in SmartHomeNG v1.6 and above
+            Available in SmartHomeNG v1.6 and above
 
-        :param value: value of the item
-        :type value: <type of the item>
+            :return: path of the item
+            :rtype: str
+            """
+            return self._item._path
 
-        :return: value of the item
-        :rtype: <type of the item>
-        """
-        return copy.deepcopy(self._value)
+        @path.setter
+        def path(self, value):
+            self._ro_error()
+            return
 
-    @ivalue.setter
-    def ivalue(self, value):
 
-        self.set(value, 'assign')
-        return
+        @property
+        def trigger(self):
+            """
+            Property: Triggers of the item
+
+            Available in SmartHomeNG v1.6 and above
+
+            :param value: list of triggers
+            :type value: list
+
+            :return: [] if not defined or a list of triggers
+            :rtype: list of str
+            """
+            if self._item._trigger:
+                return self._item._trigger
+            return []
+
+        def _checkstrtype(self, obj):
+            return bool(obj) and all(isinstance(elem, str) for elem in obj)
+
+        @trigger.setter
+        def trigger(self, value):
+
+            if isinstance(value, list):
+                if value == []:
+                    self._item._trigger = False
+                else:
+                    if self._checkstrtype(value):
+                        self._item._trigger = value
+                    else:
+                        self._type_error('list containing non-string')
+            else:
+                self._type_error('non-list')
+            return
+
+
+        @property
+        def type(self):
+            """
+            Read-Only Property: type
+
+            Available in SmartHomeNG v1.6 and above
+
+            :return: type of the item
+            :rtype: str
+            """
+            return self._item._type
+
+        @type.setter
+        def type(self, value):
+            self._ro_error()
+            return
+
+
+        @property
+        def value(self):
+            """
+            Property: value
+
+            Available in SmartHomeNG v1.6 and above
+
+            :param value: value of the item
+            :type value: <type of the item>
+
+            :return: value of the item
+            :rtype: <type of the item>
+            """
+            return copy.deepcopy(self._item._value)
+
+        @value.setter
+        def value(self, value):
+
+            self._item.set(value, 'assign property')
+            return
+
+
+    """
+    ---
+    ---  End of Properties class
+    ---
+    --------------------------------------------------------------------------------------------
+    """
+
+
 
 
     """
     Following are methods to get attributes of the item
     """
 
-    # def path(self):
-    #     """
-    #     Path of the item
-    #
-    #     Starting with SmartHomeNG v1.6
-    #
-    #     :return: String with the path of the item
-    #     :rtype: str
-    #     """
-    #     return self._path
+    def path(self):
+        """
+        Path of the item
+
+        Available only in SmartHomeNG v1.6, not in versions above
+
+        :return: String with the path of the item
+        :rtype: str
+        """
+        return self._path
 
     def id(self):
         """
@@ -899,15 +968,6 @@ class Item():
         """
         return self._path
 
-    # def name(self):
-    #     """
-    #     Name of the item
-    #
-    #     Starting with SmartHomeNG v1.6
-    #     :return: String with the name of the item
-    #     :rtype: str
-    #     """
-    #     return self._name
 
     def type(self):
         """
