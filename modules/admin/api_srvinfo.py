43b330fa62d8c5bd9b51a3e40e23d3737b38e0c1
msinn <info1@msinn.de>
2019-01-05 15:13:11 +0100

    Module admin: Initial version of services-tab

--- modules/admin/api_srvinfo.py
+++ modules/admin/api_srvinfo.py
@@ -30,6 +30,30 @@ from .rest import RESTResource
 import bin.shngversion
 
 
+
+def get_process_info(command, wait=True):
+    """
+    returns output from executing a given command via the shell.
+    """
+    ## get subprocess module
+    import subprocess
+
+    ## call date command ##
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+
+    # Talk with date command i.e. read data from stdout and stderr. Store this info in tuple ##
+    # Interact with process: Send data to stdin. Read data from stdout and stderr, until end-of-file is reached.
+    # Wait for process to terminate. The optional input argument should be a string to be sent to the child process, or None, if no data should be sent to the child.
+    (result, err) = p.communicate()
+#    logger.warning("get_process_info: command='{}', result='{}', err='{}'".format(command, result, err))
+
+    if wait:
+        ## Wait for date to terminate. Get return returncode ##
+        p_status = p.wait()
+    return str(result, encoding='utf-8', errors='strict')
+
+
+
 class ServerinfoController(RESTResource):
 
     def __init__(self, sh, module):
@@ -40,6 +64,44 @@ class ServerinfoController(RESTResource):
 
         return
 
+
+    def get_knx_daemon(self):
+        """
+        Tests it knxd or eibd are running
+        """
+        # knxd_service = get_process_info("systemctl status knxd.service")
+        # smarthome_service = get_process_info("systemctl status smarthome.service")
+        # knxd_socket = get_process_info("systemctl status knxd.socket")
+
+        daemon = 'SERVICES.INACTIVE'
+        if get_process_info("ps cax|grep eibd") != '':
+            daemon = 'eibd'
+        if get_process_info("ps cax|grep knxd") != '':
+            if daemon != 'SERVICES.INACTIVE':
+                daemon += ' and knxd'
+            else:
+                daemon = 'knxd'
+        return daemon
+
+
+    def get_1wire_daemon(self):
+        """
+        Tests it 1wire are running
+        """
+        daemon = 'SERVICES.INACTIVE'
+        if get_process_info("ps cax|grep owserver") != '':
+            daemon = 'owserver'
+        return daemon
+
+
+    def get_mqtt_daemon(self):
+        """
+        Tests it 1wire are running
+        """
+        daemon = 'SERVICES.INACTIVE'
+        if get_process_info("ps cax|grep mosquitto") != '':
+            daemon = 'mosquitto'
+        return daemon
 
 
     @cherrypy.expose
@@ -62,5 +124,8 @@ class ServerinfoController(RESTResource):
         response['plugins_branch'] = bin.shngversion.get_plugins_branch()
         response['websocket_host'] = self.module.websocket_host
         response['websocket_port'] = self.module.websocket_port
+        response['daemon_knx'] = self.get_knx_daemon()
+        response['daemon_ow'] = self.get_1wire_daemon()
+        response['daemon_mqtt'] = self.get_mqtt_daemon()
         return json.dumps(response)
 
