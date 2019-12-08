4f6dfbc7d4e6c2d031abace4e28966b6205d6da5
msinn <info2@msinn.de>
2019-12-08 22:37:08 +0100

    Module http - Webinterfaces: Initial version with support for periodic updata of data

--- modules/http/webif/gstatic/js/smarthomeng.js
+++ modules/http/webif/gstatic/js/smarthomeng.js
@@ -6,3 +6,85 @@ function resizeCodeMirror(codeMirrorInstance, bottomSpace) {
         codeMirrorInstance.refresh();
     }
 }
+
+
+/**********************************************************************
+ * following scripts are for usage in webinterfaces of plugins.
+ */
+
+
+/**
+ * sends a request to the specified url from a form. this will change the window location.
+ * @param {string} path the path to send the post request to
+ * @param {object} params the paramiters to add to the url
+ * @param {string} [method=post] the method to use on the form
+ */
+
+function shngPost(path, params, method='post') {
+
+  // The rest of this code assumes you are not using a library.
+  // It can be made less wordy if you use one.
+  const form = document.createElement('form');
+  form.method = method;
+  form.action = path;
+
+  for (const key in params) {
+    if (params.hasOwnProperty(key)) {
+      const hiddenField = document.createElement('input');
+      hiddenField.type = 'hidden';
+      hiddenField.name = key;
+      hiddenField.value = params[key];
+
+      form.appendChild(hiddenField);
+    }
+  }
+
+  document.body.appendChild(form);
+  form.submit();
+}
+
+
+/**
+ * inserts text into a dom element. To be used for ajax updates
+ * @param {string} id of the dom element
+ * @param {string} text to insert
+ */
+
+function shngInsertText (id, text) {
+    document.getElementById(id).innerHTML = text;
+}
+
+
+/**
+ * fires an ajax request to get actual data from the plugin
+ * @param {string} optional name of dataset to get. Only needed, if the webinterface gets multiple different datasets from the plugin
+ */
+function shngGetUpdatedData(dataSet=null) {
+    if (dataSet) {
+    $.ajax({
+        url: "get_data.html",
+        type: "GET",
+        data: { dataSet : dataSet
+              },
+        contentType: "application/json; charset=utf-8",
+        success: function (response) {
+                handleUpdatedData(response, dataSet);
+        },
+        error: function () {
+            console.warn("Error - while getting updated data for dataSet :"+dataSet)
+        }
+    });
+    } else {
+    $.ajax({
+        url: "get_data.html",
+        type: "GET",
+        contentType: "application/json; charset=utf-8",
+        success: function (response) {
+                handleUpdatedData(response);
+        },
+        error: function () {
+            console.warn("Error - while getting updated data")
+        }
+    });
+    }
+}
