5af4cb0a7f94fd04cb8cd6c003ce787d1c40d3db
psilo909 <rene.friess@gmx.de>
2020-06-15 08:47:21 +0200

    sample_plugin: removed import and initialisation of logger

--- dev/sample_plugin/webif/__init__.py
+++ dev/sample_plugin/webif/__init__.py
@@ -28,7 +28,6 @@
 import datetime
 import time
 import os
-import logging
 
 from lib.item import Items
 from lib.model.smartplugin import SmartPluginWebIf
@@ -54,7 +53,6 @@ class WebInterface(SmartPluginWebIf):
         :type webif_dir: str
         :type plugin: object
         """
-        self.logger = logging.getLogger(__name__)
         self.webif_dir = webif_dir
         self.plugin = plugin
         self.tplenv = self.init_template_environment()
